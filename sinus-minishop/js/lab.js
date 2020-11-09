22dea6cb1ce78d72889be7c7171de650e865da74
Marcus Järveskog <abr.metalcore98@gmail.com>
2020-11-09 16:48:08 +0100

    almost done, needs som finetuning and eventlistners

--- sinus-minishop/js/lab.js
+++ sinus-minishop/js/lab.js
@@ -12,9 +12,12 @@ reset.innerText = 'RESET'
 
 // reset functionen
 function resetAll(){
-    // reset art1 background
-    let art1 = document.querySelector('.art-1');
-    art1.style.backgroundColor = 'white'
+    // reset articles colors
+    let mainArticles = document.querySelectorAll('main article');
+    for(i = 0; i < mainArticles.length; i++){
+        mainArticles[i].style.backgroundColor = "#ffffff";
+        mainArticles[i].style.color = "#000000"
+    }
 
     // reset art2 h2
     let h2 = document.querySelector('.art-2 h2');
@@ -41,6 +44,30 @@ function resetAll(){
     if(firstMenuItem.innerText == "Products"){
         firstMenuItem.insertAdjacentElement('beforebegin', menuItem)
     }
+
+    // reset footer content
+    let footer = document.querySelector('footer');
+    footer.innerHTML = `
+    <section>
+    <article>
+        <h3>Menu</h3>
+        <a href="#">Home</a>
+        <a href="#">Products</a>
+        <a href="#">Contact</a>
+    </article>
+    <article>
+        <h3>Where we are</h3>
+        <p>Sinus skateboards <br>
+        Railsvägen 13 <br>
+        133 37, Rampnäs</p>
+    </article>
+    <article>
+        <h3>Social media</h3>
+        <a href="#">Twitter</a>
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+    </article>
+</section>`
     
     
 }
@@ -57,20 +84,31 @@ let darkModeBtn = btns[0];
 darkModeBtn.innerText = "Dark"
 darkModeBtn.addEventListener('click', 
     function(e){
-        changeArt1ToGrey();
-        changeH2inArt2();
-        changeImgArt2();
+        changeArtToGrey();
         redButton();
         changeArt2Text();
         removeMenuItems();
     }
 );
 
+// event på knappen i mitten för att ta bort footer och lägga till y list
+let middleBtn = document.querySelector('.art-2 button');
+middleBtn.addEventListener('click', function(e){
+    removeFooterContent();
+    addList();
+});
+
+//event på bilden i mitten för att byta bild
+let middleImg = document.querySelector('.art-2 img');
+middleImg.addEventListener('click', function(e){
+    changeImgArt2();
+    changeH2inArt2();
+})
+
 // byt ut bilden på article 2
 function changeImgArt2(){
-    let art2Img = document.querySelector('.art-2 img');
-    art2Img.src = "https://i.pinimg.com/originals/5c/85/0c/5c850c07dbeb97237a1ffa9975c125c6.png"
-    art2Img.style.width = "70%"
+    middleImg.src = "https://i.pinimg.com/originals/5c/85/0c/5c850c07dbeb97237a1ffa9975c125c6.png"
+    middleImg.style.width = "70%"
 }
 
 
@@ -81,10 +119,14 @@ function changeH2inArt2(){
 }
 
 
-// function som ändrar art-1 färg till mörkgrå
-function changeArt1ToGrey(){
-    let art1 = document.querySelector('.art-1');
-    art1.style.backgroundColor = '#4e4f4f'
+// function som ändrar mainArticles backgrunds färger till mörkgrå och text färg till vit
+function changeArtToGrey(){
+    let mainArticles = document.querySelectorAll('main article');
+    for(i = 0; i < mainArticles.length; i++){
+        mainArticles[i].style.backgroundColor = "red";
+        mainArticles[i].style.color = "#ffffff"
+    }
+
 }
 
 // ändra färg på knappen i art1
@@ -105,6 +147,34 @@ function removeMenuItems(){
     let menuItem = document.querySelector('#header-navigation a')
     if(menuItem.innerText == "Home"){
         menu.removeChild(menuItem);
+    } 
+}
+
+// ta bort footer listor
+function removeFooterContent(){
+    let footerSection = document.querySelector('footer section');
+    footerSection.remove();
+}
+
+
+
+//skapa och lägg till en en ny lista på sidan
+function addList(){
+    let newList = document.createElement('ul')
+    let listItem = document.createElement('li')
+    let footer = document.querySelector('footer');
+    footer.appendChild(newList)
+
+    // loopa fram 5 list items
+    for(let i = 1; i <= 5; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = `Epic List Item ${i}`;
+        newList.appendChild(li);
+
+        li.style.listStyle = "none";
+        li.style.borderBottom = "solid crimson"
+        li.style.width = "100vw"
+        li.style.textAlign = "center"
+        li.style.color = "white"
     }
-    
 }
